4d165fc7769b5992184f110875e7400d4e1bc2cc
Nima T <nima.traore@obspm.fr>
2016-01-29 19:16:38 +0100

    sortie fichier format vts ephemeride ds ts_test

--- config.py
+++ config.py
@@ -7,9 +7,16 @@ log_dir    = "/home/birdy/Software/TS_tests/results/"
 host_trajectory_file = "host_traj.xyzv"
 output_trajectory_file = "result.traj"
 output_ephemeris_file = "ephemeris.traj"
+
+#------------- Nima
+output_ephemeris_vts_format_file = "ephemeris_vts_format.traj"
+
+#-----------------EOM---
+
+
 jettison_index = 0
 interval = 1./4    # Unit: day (e.g. 1./24 means one step per hour)
-steps = 20        # How many steps basing on interval you want? (e.g. 24 steps * 1.24(interval) = 1 day )
+steps = 5        # How many steps basing on interval you want? (e.g. 24 steps * 1.24(interval) = 1 day )
 show_plot = "False" # True or False
 refine_pass = 30    # Maximum number of "fmin_cobyla" function evaluations.
 
